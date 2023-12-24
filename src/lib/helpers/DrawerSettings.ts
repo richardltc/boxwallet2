8067f2bc3b0a8b3de97cdda238bd1ca9bfc4f68f
Richard Mace <richard.mace@gmail.com>
2023-12-24 07:50:21 +0000

    Added helper

---
+++ src/lib/helpers/DrawerSettings.ts
@@ -0,0 +1,11 @@
+import type { DrawerSettings } from '@skeletonlabs/skeleton';
+
+export const Drawer_Settings: DrawerSettings = {
+	position: 'right',
+	// Provide your property overrides:
+	// bgDrawer: 'bg-purple-900 text-white',
+	bgBackdrop: 'bg-surface-500/20',
+	width: 'w-[580px] md:w-[580px]',
+	padding: 'p-4',
+	rounded: 'rounded-xl'
+};
