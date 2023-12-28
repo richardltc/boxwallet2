cbc1903736a8e388c62cccf14c1ccbacc0729887
Richard Mace <richard.mace@gmail.com>
2023-12-28 07:23:39 +0000

    Added new GetPassword component

---
+++ src/lib/GetPassword.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { getModalStore, type ModalSettings } from '@skeletonlabs/skeleton';
+
+	export let resolveFunction: (value: string) => void;
+	const modalStore = getModalStore();
+
+
+	const modal: ModalSettings = {
+		type: 'confirm',
+		title: 'Enter password to unlock wallet',
+		body: 'Enter password?',
+		response: (r: string) => {
+			resolveFunction(r);
+		},
+	};
+
+	onMount(() => {
+		// Assuming modalStore is your store for managing modals
+		modalStore.trigger(modal);
+	});
+</script>
