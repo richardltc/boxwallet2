385fce5aa99fc082740ebf04ded402f4d4439522
Richard Mace <richard.mace@gmail.com>
2024-01-13 07:47:43 +0000

    Renamed GetInfo to GetNetworkInfo

--- src/lib/rdd_getnetworkinfo_store.ts
+++ src/lib/rdd_getnetworkinfo_store.ts
@@ -1,5 +1,5 @@
 import { writable } from 'svelte/store';
 
-// From GetInfo
+// From GetNetworkInfo API
 export const walletConnections = writable(0);
 export const walletUnlockedUntil = writable(-5);
