e4300200510f70751507ca16f027eae0e0a59be6
Richard Mace <richard.mace@gmail.com>
2024-01-13 07:46:16 +0000

    Added initial BlockchainHeaders component

---
+++ src/lib/BlockchainHeaders.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { headers } from '$lib/rdd_getblockchaininfo_store.js';
+
+    let header_height: number;
+
+    const unsub_headers = headers.subscribe((value) => {
+        header_height = value
+
+    })
+
+</script>
+
+<main>
+</main>
+
+<style>
+</style>
