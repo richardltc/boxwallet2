49f46cebce4a27e93e1451dc1a1c250bdb5f9a9c
Richard Mace <richard.mace@gmail.com>
2024-01-15 13:41:03 +0000

    Formatted and added some animation.

--- src/lib/BlockchainHeaders.svelte
+++ src/lib/BlockchainHeaders.svelte
@@ -1,20 +1,25 @@
 <script lang="ts">
     import { headers } from '$lib/rdd_getblockchaininfo_store.js';
 		import { CoinMethodType } from '$lib/bwtypes';
+		import { tweened } from 'svelte/motion';
 
-    let header_height: number;
+    //let header_height: number;
+		let header_height = tweened(0, {
+			duration: 3000,
+			easing: cubicOut
+		});
+		import { cubicOut } from 'svelte/easing';
 
     const unsub_headers = headers.subscribe((value) => {
-        header_height = value
+        $header_height = value
 
     })
 
 </script>
-
 <main>
 	<div class="container mx-auto p-8 space-y-4">
 		<p>
-			Headers: {header_height.toLocaleString()}
+			Headers: {Math.round($header_height).toLocaleString()}
 		</p>
 	</div>
 
