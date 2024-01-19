44f9e59abdce8bdb67426968f304a8ca2a6bccdc
Richard Mace <richard.mace@gmail.com>
2024-01-19 19:41:05 +0000

    Removed redundant GetInfo

--- src/lib/rdd_types.ts
+++ src/lib/rdd_types.ts
@@ -14,34 +14,34 @@ interface GetBlockchainInfoResult {
 	chainwork: string;
 }
 
-export interface GetInfoResponse {
-	result: GetInfoResult;
-	error: any;
-	id: string;
-}
+// export interface GetInfoResponse {
+// 	result: GetInfoResult;
+// 	error: any;
+// 	id: string;
+// }
 
-interface GetInfoResult {
-	version: number;
-	protocolversion: number;
-	walletversion: number;
-	balance: number;
-	stake: number;
-	locked: boolean;
-	encrypted: boolean;
-	blocks: number;
-	timeoffset: number;
-	moneysupply: number;
-	connections: number;
-	proxy: string;
-	difficulty: number;
-	testnet: boolean;
-	keypoololdest: number;
-	keypoolsize: number;
-	unlocked_until: number;
-	paytxfee: number;
-	relayfee: number;
-	errors: string;
-}
+// interface GetInfoResult {
+// 	version: number;
+// 	protocolversion: number;
+// 	walletversion: number;
+// 	balance: number;
+// 	stake: number;
+// 	locked: boolean;
+// 	encrypted: boolean;
+// 	blocks: number;
+// 	timeoffset: number;
+// 	moneysupply: number;
+// 	connections: number;
+// 	proxy: string;
+// 	difficulty: number;
+// 	testnet: boolean;
+// 	keypoololdest: number;
+// 	keypoolsize: number;
+// 	unlocked_until: number;
+// 	paytxfee: number;
+// 	relayfee: number;
+// 	errors: string;
+// }
 
 export interface GenericResponse {
 	result: string;
