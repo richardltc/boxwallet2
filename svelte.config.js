91e051f93342d2c2677a0acb3b8a36ba9e107405
Richard Mace <richard.mace@gmail.com>
2024-01-25 17:09:36 +0000

    Updated

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
+import { preprocessMeltUI, sequence } from '@melt-ui/pp';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import('@sveltejs/kit').Config}*/
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	
+	preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
 	vitePlugin: {
-		inspector: true,   
+		inspector: true
 	},
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
