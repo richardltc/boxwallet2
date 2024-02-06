cd24393efcd0146428eca78a4c8e9f117e1582c8
Richard Mace <richard.mace@gmail.com>
2024-02-06 17:46:47 +0000

    Added more reaction to stores

--- src/lib/bw_store.ts
+++ src/lib/bw_store.ts
@@ -2,5 +2,6 @@ import { writable } from 'svelte/store';
 import { CoreFileStatusType, WalletRunningStatusType } from '$lib/bwtypes';
 
 // Various BoxWallet states.
-export const coreFileStatus = writable(CoreFileStatusType.not_installed);
+export const coreFileStatus = writable(CoreFileStatusType.cfst_not_installed);
+export const isWorking = writable(false);
 export const walletRunningStatus = writable(WalletRunningStatusType.wrst_stopped);
