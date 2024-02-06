be401c4a0ddcb0f0db482704bbd7900947ffb4fc
Richard Mace <richard.mace@gmail.com>
2024-02-06 07:24:25 +0000

    Updated

---
+++ src/lib/components/WalletVersion.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { walletVersion } from '$lib/rdd_getnetworkinfo_store';
+	import { tweened } from 'svelte/motion';
+
+	let wallet_version = tweened(0, {
+		duration: 5000,
+		easing: cubicOut
+	});
+	import { cubicOut } from 'svelte/easing';
+
+	const unsub_walletVersion = walletVersion.subscribe((value) => {
+		$wallet_version = value;
+	});
+</script>
+
+<main>
+	<!--	<figure class="bg-opacity-90 rounded-xl p-2 dark:bg-opacity-90">-->
+	<figure class="p-2 pr-10">
+		<div class="label">
+			{#if $wallet_version === 0}
+				version: <div class="version">...</div>
+			{:else}
+				version: <div class="version">{wallet_version}</div>
+			{/if}
+		</div>
+	</figure>
+</main>
+
+<style>
+    .version {
+        font-family: 'Courier Prime', monospace;
+        font-size: 1.2em;
+    }
+    .label {
+        font-size: 1.0em;
+    }
+
+</style>
