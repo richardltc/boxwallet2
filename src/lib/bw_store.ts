55039463cf3282a4a584a049d326dc2743e88583
Richard Mace <richard.mace@gmail.com>
2024-02-06 18:00:56 +0000

    Added daemonRunningStatus

--- src/lib/bw_store.ts
+++ src/lib/bw_store.ts
@@ -1,7 +1,8 @@
 import { writable } from 'svelte/store';
-import { CoreFileStatusType, WalletRunningStatusType } from '$lib/bwtypes';
+import { CoreFileStatusType, DaemonRunningStatusType, WalletRunningStatusType } from '$lib/bwtypes';
 
 // Various BoxWallet states.
 export const coreFileStatus = writable(CoreFileStatusType.cfst_not_installed);
+export const daemonRunningStatus = writable(DaemonRunningStatusType.drstStopped);
 export const isWorking = writable(false);
 export const walletRunningStatus = writable(WalletRunningStatusType.wrst_stopped);
