715dc82e264f26588625bf465954d095c43de2f0
Richard Mace <richard.mace@gmail.com>
2024-02-19 13:17:47 +0000

    Added verificationProgress

--- src/lib/rdd_getblockchaininfo_store.ts
+++ src/lib/rdd_getblockchaininfo_store.ts
@@ -1,6 +1,7 @@
 import { writable } from 'svelte/store';
 
-// From GetBlockchainInfo API.
+// From GetBlockchainInfo API
 export const blocks = writable(0);
 export const difficulty = writable(0);
 export const headers = writable(0);
+export const verificationProgress = writable(0);
