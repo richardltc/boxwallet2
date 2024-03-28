c606c1d88346d770d234ba24527b74929bb2e1fc
Richard Mace <richard.mace@gmail.com>
2024-03-28 17:26:10 +0000

    Updated from number to string

--- src/lib/components/WalletVersion.svelte
+++ src/lib/components/WalletVersion.svelte
@@ -1,18 +1,15 @@
 <script lang="ts">
 	// import { walletVersion } from '$lib/rdd/rdd_getnetworkinfo_store';
 
-	export let wallet_version = 0;
+	export let wallet_version = "";
 
-	//const unsub_walletVersion = walletVersion.subscribe((value) => {
-	//	wallet_version = value;
-	//});
 </script>
 
 <main>
 	<!--	<figure class="bg-opacity-90 rounded-xl p-2 dark:bg-opacity-90">-->
 	<figure class="p-2 pr-10">
 		<div class="label">
-			{#if wallet_version === 0}
+			{#if wallet_version === ""}
 				v<span class="version">...</span>
 			{:else}
 				v: <span class="version">{wallet_version}</span>
