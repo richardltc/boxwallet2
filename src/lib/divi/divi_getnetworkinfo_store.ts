9923d17eef72b69e0af577140a2dcd8e777c3b80
Richard Mace <richard.mace@gmail.com>
2024-03-28 17:27:06 +0000

    Wallet version work

--- src/lib/divi/divi_getnetworkinfo_store.ts
+++ src/lib/divi/divi_getnetworkinfo_store.ts
@@ -1,6 +1,5 @@
 import { writable } from 'svelte/store';
 
-// From GetNetworkInfo API for Divi.
+// From GetNetworkInfo API for Divi
 export const walletConnections = writable(0);
-export const walletUnlockedUntil = writable(-5);
-export const coinWalletVersion = writable(0);
+export const coinWalletVersion = writable('');
