abf1c458d7c590fcd1019c57074b742b2acd7849
Richard <richard.mace@gmail.com>
2025-08-15 22:34:08 +0100

    Correct App module

--- lib/boxwallet/app.ex
+++ lib/boxwallet/app.ex
@@ -1,14 +1,14 @@
-@app_name       "BoxWallet"
-@updater_app_name "bwupdater"
-@app_version      "0.0.1"
-@app_filename     "boxwallet"
-@app_filename_win "boxwallet.exe"
-
-@app_working_dir_lin ".boxwallet"
-@app_working_dir_lin  "BoxWallet"
-
 defmodule BoxWallet.App do
-  def Name do
+  @app_name       "BoxWallet"
+  @updater_app_name "bwupdater"
+  @app_version      "0.0.1"
+  @app_filename     "boxwallet"
+  @app_filename_win "boxwallet.exe"
 
+  @app_working_dir_lin ".boxwallet"
+  @app_working_dir_lin  "BoxWallet"
+
+  def name do
+    @app_name
   end
 end
