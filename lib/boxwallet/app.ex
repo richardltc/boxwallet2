50c44669e9c4a142117692abeea112934972dc2b
Richard <richard.mace@gmail.com>
2025-08-15 22:24:18 +0100

    Added App module

---
+++ lib/boxwallet/app.ex
@@ -0,0 +1,14 @@
+@app_name       "BoxWallet"
+@updater_app_name "bwupdater"
+@app_version      "0.0.1"
+@app_filename     "boxwallet"
+@app_filename_win "boxwallet.exe"
+
+@app_working_dir_lin ".boxwallet"
+@app_working_dir_lin  "BoxWallet"
+
+defmodule BoxWallet.App do
+  def Name do
+
+  end
+end
