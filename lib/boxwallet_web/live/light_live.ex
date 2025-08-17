a5dcbe0fd8d7e43dc982ea277907d251d8fa26e8
Richard <richard.mace@gmail.com>
2025-08-17 08:53:52 +0100

    Divi download to correct directory

--- lib/boxwallet_web/live/light_live.ex
+++ lib/boxwallet_web/live/light_live.ex
@@ -1,4 +1,5 @@
 defmodule BoxwalletWeb.LightLive do
+  import BoxWallet.App
   use BoxwalletWeb, :live_view
   alias Boxwallet.Coins.Divi
 
@@ -8,21 +9,21 @@ defmodule BoxwalletWeb.LightLive do
   end
 
   def handle_event("download_divi", _, socket) do
-    IO.puts("Download Divi button clicked")
-    Divi.download_coin(System.tmp_dir!())
+    IO.puts("Download Divi button clicked - Downloading to #{BoxWallet.App.home_folder}")
+    Divi.download_coin(BoxWallet.App.home_folder)
     {:noreply, socket}
   end
 
   def render(assigns) do
     ~H"""
-    <h1>Light Control</h1>
-    <div id="light">
+    <h1>Divi Core Test Page</h1>
+    <%!-- <div id="light">
       <div class="meter">
         <span style={"width: #{@brightness}%"}>
           <%= assigns.brightness %>%
         </span>
       </div>
-    </div>
+    </div> --%>
     <button phx-click="download_divi">Download Divi</button>
     """
   end
