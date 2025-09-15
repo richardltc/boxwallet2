8c57378b72a8c60bce3956d62b75ca4dc644647e
Richard <richard.mace@gmail.com>
2025-09-15 14:17:03 +0100

    Removed

--- lib/boxwallet/coins/litecoin.ex
+++ lib/boxwallet/coins/litecoin.ex
@@ -3,7 +3,7 @@ defmodule Boxwallet.Coins.Litecoin do
   @behaviour Boxwallet.CoinDaemon
   # Replace with actual URL
   @daemon_url "https://example.com/litecoind.tar.gz"
-  @install_path Path.expand("~/.my_app/litecoin")
+  # @install_path Path.expand("~/.my_app/litecoin")
   @rpc_url "http://localhost:9332"
   @rpc_credentials [username: "rpcuser", password: "rpcpass"]
 end
