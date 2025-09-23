70767eed0a0f9e0997bf88de5bd32ecb8e19674c
Richard <richard.mace@gmail.com>
2025-09-23 18:36:40 +0100

    Formatting

--- config/prod.exs
+++ config/prod.exs
@@ -6,8 +6,8 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :boxwallet, BoxwalletWeb.Endpoint,
-cache_static_manifest: "priv/static/cache_manifest.json",
-server: true
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true
 
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Req
