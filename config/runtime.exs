1697d08702ebd96252bfebe45dead1a711938c68
Richard <richard.mace@gmail.com>
2025-12-20 08:05:06 +0000

    Improved runtime.exs config for automatic secret creation, to make it easier for users to get up and running.

--- config/runtime.exs
+++ config/runtime.exs
@@ -26,12 +26,27 @@ if config_env() == :prod do
   # want to use a different value for prod and you most likely don't want
   # to check this value into version control, so we use an environment
   # variable instead.
+
+  # secret_key_base =
+  #   System.get_env("SECRET_KEY_BASE") ||
+  #     raise """
+  #     environment variable SECRET_KEY_BASE is missing.
+  #     You can generate one by calling: mix phx.gen.secret
+  #     """
+  #
+  # # Generate or load a persistent secret key
+  secret_file = Path.join([System.get_env("RELEASE_ROOT", "."), "secret_key_base.txt"])
+
   secret_key_base =
-    System.get_env("SECRET_KEY_BASE") ||
-      raise """
-      environment variable SECRET_KEY_BASE is missing.
-      You can generate one by calling: mix phx.gen.secret
-      """
+    if File.exists?(secret_file) do
+      File.read!(secret_file) |> String.trim()
+    else
+      # Generate a new secret
+      secret = :crypto.strong_rand_bytes(64) |> Base.encode64(padding: false)
+      File.write!(secret_file, secret)
+      IO.puts("Generated new secret key at: #{secret_file}")
+      secret
+    end
 
   host = System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
@@ -45,7 +60,8 @@ if config_env() == :prod do
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      # ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      ip: {0, 0, 0, 0},
       port: port
     ],
     secret_key_base: secret_key_base
