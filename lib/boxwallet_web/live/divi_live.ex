e3bd295bff1b6af537c67307f267d5691c068197
Richard <richard.mace@gmail.com>
2026-02-17 09:04:44 +0000

    Updated start and stop buttons

--- lib/boxwallet_web/live/divi_live.ex
+++ lib/boxwallet_web/live/divi_live.ex
@@ -770,21 +770,7 @@ defmodule BoxwalletWeb.DiviLive do
             disabled={!@coin_files_exist or !@coin_daemon_stopped}
             title={"Start #{@coin_name} Daemon"}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="size-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
-              />
-            </svg>
-            Start
+            <span class="hero-play h-6 w-6" /> Start
           </button>
 
           <button
@@ -793,21 +779,7 @@ defmodule BoxwalletWeb.DiviLive do
             disabled={!@coin_daemon_started}
             title={"Stop #{@coin_name} Daemon"}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="size-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M11.412 15.655 9.75 21.75l3.745-4.012M9.257 13.5H3.75l2.659-2.849m2.048-2.194L14.25 2.25 12 10.5h8.25l-4.707 5.043M8.457 8.457 3 3m5.457 5.457 7.086 7.086m0 0L21 21"
-              />
-            </svg>
-            Stop
+            <span class="hero-stop h-6 w-6" /> Stop
           </button>
 
           <button
