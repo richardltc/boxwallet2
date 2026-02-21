87a7a37535f7c55a08de3b3f6b76fd2bf7338787
Richard <richard.mace@gmail.com>
2026-02-21 23:17:45 +0000

    Added wallet_encrypt function

--- lib/boxwallet/coins/divi/divi.ex
+++ lib/boxwallet/coins/divi/divi.ex
@@ -758,6 +758,45 @@ defmodule Boxwallet.Coins.Divi do
     end)
   end
 
+  def wallet_encrypt(auth, password) do
+    body =
+      Jason.encode!(%{
+        jsonrpc: "1.0",
+        id: "curltext",
+        method: "encryptwallet",
+        params: ["#{password}"]
+      })
+
+    url = "http://127.0.0.1:#{auth.rpc_port}"
+
+    headers = [
+      {"Content-Type", "text/plain"},
+      {"Authorization", "Basic #{Base.encode64("#{auth.rpc_user}:#{auth.rpc_password}")}"}
+    ]
+
+    Logger.info("Attempting to Encrypt wallet")
+
+    case HTTPoison.post(url, body, headers) do
+      {:ok, %{body: response_body}} ->
+        case Jason.decode(response_body) do
+          {:ok, %{"error" => nil}} ->
+            :ok
+
+          {:ok, %{"error" => %{"message" => message}}} ->
+            {:error, message}
+
+          {:ok, %{"error" => error}} ->
+            {:error, inspect(error)}
+
+          {:error, _} ->
+            {:error, "Failed to parse response"}
+        end
+
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        {:error, %HTTPoison.Error{reason: reason}}
+    end
+  end
+
   def get_sync_info do
     try do
       headers = [
