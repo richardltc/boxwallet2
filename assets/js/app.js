885540214b3d3d6be82f8d62228c76b7d856e51f
Richard <richard.mace@gmail.com>
2026-02-21 22:34:06 +0000

    Formatting

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,6 +26,7 @@ import {hooks as colocatedHooks} from "phoenix-colocated/boxwallet"
 import topbar from "../vendor/topbar"
 
 const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
+
 const liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
   params: {_csrf_token: csrfToken},
