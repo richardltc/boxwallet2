594c3629366bc0187eea2a17110ac885f086bebf
Richard <richard.mace@gmail.com>
2026-02-23 20:41:32 +0000

    Reduced a bit more.

--- lib/boxwallet_web/components/layouts.ex
+++ lib/boxwallet_web/components/layouts.ex
@@ -62,7 +62,7 @@ defmodule BoxwalletWeb.Layouts do
       </div>
     </header>
 
-    <main class="px-4 pt-4 pb-20 sm:px-6 lg:px-8">
+    <main class="px-4 pt-2 pb-20 sm:px-6 lg:px-8">
       <div class="mx-auto max-w-2xl space-y-4">
         {render_slot(@inner_block)}
       </div>
